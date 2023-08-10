13a59341b9dff846313dbff10c63de9788a58271
Ankush Malik <er.ankushmalik@gmail.com>
2023-08-10 13:52:25 +0530

    react-window

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { faker } from "@faker-js/faker";
+import { FixedSizeList as List } from "react-window";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const data = new Array(1000).fill().map((value, id) => ({
+    id: id,
+    title: faker.lorem.words(5),
+    body: faker.lorem.sentences(8),
+  }));
+
+  const Row = ({ index, key, style }) => (
+    <div>
+      <div key={key} style={style} className="post">
+        <h3>{`${data[index].title}-${data[index].id}`}</h3>
+        <p>{data[index].body}</p>
+      </div>
     </div>
   );
+  return (
+    <List width={1400} height={700} itemCount={data.length} itemSize={120}>
+      {Row}
+    </List>
+  );
 }
-
 export default App;
